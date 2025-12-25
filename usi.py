428a8f72bb5308955473d68f9cf1b961874fbd32
Bhawna Kapoor <bhawnakapoor2001@gmail.com>
2025-12-25 23:35:06 +0530

    Add argparse subcommands for listing and checking service status

--- usi.py
+++ usi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Code using argparse'''
+
 import argparse
 import subprocess
 import json
@@ -8,72 +8,51 @@ def main():
     # 1. Initialising the parser 
     parser = argparse.ArgumentParser(description="Ubuntu Service Inspector (usi)")
 
-    # 2. Add the 'command' argument
-    # By setting 'choices', argparse will automatically reject anything except 'list'.
-    parser.add_argument(
-        "command",
-        choices=["list"],
-        help="The action to perform (e.g., list)"
-    )
+    # 2. Create a 'subparser' object
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
-    # 3. Process the input
+    # 3. Define the 'list' command sub-tool
+    list_parser = subparsers.add_parser(
+        "list",
+        help="List all services"
+    )
+    list_parser.add_argument("--failed", action="store_true", help="Show only failed")
+    list_parser.add_argument("--no-pager", action="store_true", help="Print all output to stdout")
+
+    # 4. Define the 'status' command sub-tool
+    status_parser = subparsers.add_parser("status", help="Check the status of a specific service")
+    status_parser.add_argument("service_name", help="Check bluetooth of a service")
+
+    # 5. Process the input
     # If the user provides no command, argparse will print a help message and exit here.
     args = parser.parse_args()
     # The value is now stored inside the 'args' object.
     print(f"usi is running: {args.command}")
+    user_input = ["systemctl"]
 
+    # if command given by the user is 'list'
+    if args.command == 'list':
+        user_input = user_input + ['list-units', '--type=service', '--output=json']
+        if args.failed:
+            user_input.append('--failed')
+        if args.no_pager:
+            user_input.append('--no-pager')
+    
+    # if command given is 'status'
+    elif args.command == 'status':
+        user_input = user_input + ['status', args.service_name]
     # using subprocess.run logic using args object
     result = subprocess.run(
-        [
-            "systemctl",
-            args.command,
-            "--type=service",
-            "--all",
-            "--no-pager",
-            "--no-legend",
-            "--output=json"
-        ],
+        user_input,
         capture_output=True,
         text=True
-            )
-    services = json.loads(result.stdout)
-    print(services[0])
-    
+        )
 
-'''Code uses sys.argv -> Manual Code'''
-# import sys
-# import subprocess
-# import json
-
-# # print("Arguments passed:", sys.argv)
-# def main():
-#     print("usi is running")
-#     if len(sys.argv) < 2:
-#         print("Error: missing command")
-#         sys.exit(1)
-    
-#     command = sys.argv[1]
-
-#     if command != "list":
-#         print(f"Unknown command: {command}")
-#         sys.exit(1)
-
-#     print("Listing servises....")
-#     result = subprocess.run(
-#         [
-#             "systemctl",
-#             "list-units",
-#             "--type=service",
-#             "--all",
-#             "--no-pager",
-#             "--no-legend",
-#             "--output=json"
-#         ],
-#         capture_output=True,
-#         text=True
-#     )
-#     services = json.loads(result.stdout)
-#     print(services[0])
+    if args.command == 'list':
+        services = json.loads(result.stdout)
+        print(services[0])
+    elif args.command == 'status':
+        print(f"status of {args.service_name} is: {result.stdout}")
 
 
 if __name__=="__main__":
